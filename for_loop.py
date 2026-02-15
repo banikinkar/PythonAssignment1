8cac19abf3916564d3b2c4a45b977b889d086550
banikinkar <Kinkar.pani@gmail.com>
2026-02-15 17:45:00 +0530

    looping practise

--- for_loop.py
+++ for_loop.py
@@ -9,8 +9,21 @@ for i in l1:
 
     for x in reversed(range(1,11)):  # for x in range (1,11):
         print(x)
-"""
+
 
 s = 'Bani Kinkar Pani'
 for i in s:
     print(i)
+print("end of loop")
+"""
+student1 = {"name":"John" , "age":30, "subject":"Eng"}
+
+#for i in student1:
+ #   print(i,student1[i])
+#print(student1.items())
+for i in student1.items():
+    print(i[0], i[1])
+
+
+
+
