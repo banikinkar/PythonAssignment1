5f1183466ff6dda01ba27301ec7648facfed7357
banikinkar <Kinkar.pani@gmail.com>
2026-02-21 10:12:40 +0530

    recursive & function as arg  function

--- functions/recursion.py
+++ functions/recursion.py
@@ -1,15 +1,23 @@
 ## factorial of a number
+
 def fact_rec(num):
-    """
-    computes the factorial of a given number
-    :param num:
-    :return:
-    """
+
     if num == 0:
         return 1
     else:
         return num * fact_rec(num-1)
 
 print(fact_rec(5))
+
+
+def add(num):
+    num +=1
+    return num
+
+def square(num):
+     num =num**2
+     return num
+
+print(square(add(3)))
 
 
