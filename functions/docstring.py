0b422eb4fa038fa89b08f06e5093ed76cfd27440
banikinkar <Kinkar.pani@gmail.com>
2026-02-21 09:20:10 +0530

    docstring and --doc--

---
+++ functions/docstring.py
@@ -0,0 +1,17 @@
+def docstring_funct(num1,num2):
+    """
+    This is a docstring function
+    :return: none
+    """
+    # sum = num1+num2
+    # avg=num1/num2
+    # div=num1*num1
+
+
+    return None
+
+# print(help(docstring_funct))
+print(docstring_funct.__doc__)
+
+
+
