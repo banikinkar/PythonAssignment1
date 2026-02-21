6722b5d264f10e86ad27c35f9ee91453f9c9597a
banikinkar <Kinkar.pani@gmail.com>
2026-02-21 22:13:03 +0530

    module built in and user defined

---
+++ functions/module.py
@@ -0,0 +1,12 @@
+#import math
+import random
+import datetime
+from random import choice as ch
+from math import sqrt
+# print(math.sqrt(6))
+# print(math.pi)
+print(sqrt(5))
+print(ch(["a", "b", "c"]))
+
+def add(a, b):
+    return a + b
