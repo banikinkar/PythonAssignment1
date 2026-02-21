d728a7331d83dde8bb581faa875bf28dfaecc990
banikinkar <Kinkar.pani@gmail.com>
2026-02-21 22:31:43 +0530

    __name__ and __main__

--- functions/module.py
+++ functions/module.py
@@ -5,8 +5,16 @@ from random import choice as ch
 from math import sqrt
 # print(math.sqrt(6))
 # print(math.pi)
-print(sqrt(5))
-print(ch(["a", "b", "c"]))
+# print(sqrt(5))
+# print(ch(["a", "b", "c"]))
 
 def add(a, b):
     return a + b
+
+if "__name__" == "__main__":
+    x =10
+    y=20
+    result = add(x,y)
+    print(result)
+    print(sqrt(5))
+    print(ch(["a", "b", "c"]))
--- functions/mycode.py
+++ functions/mycode.py
@@ -2,3 +2,6 @@ import module
 
 module.add(3,4)
 print(module.add(3,4))
+
+print(f"__name__ value in mycode is => {__name__}")
+print(f"__name__ value in module is => {module.__name__}")
