333da1a195ece18e0407f2ae722d3684d8cbd70b
banikinkar <Kinkar.pani@gmail.com>
2026-02-21 18:13:30 +0530

    lambda,map

---
+++ functions/lambdafunction.py
@@ -0,0 +1,17 @@
+# def odd_even(num):
+#     if num > 0:
+#        if num %2 ==0:
+#           print("Even")
+#        else:
+#           print("Odd")
+#     else:
+#         print("enter correct number")
+#
+# odd_even(0)
+
+#def odd_even((x):
+
+
+odd_even = lambda x: x % 2 == 0
+result = odd_even(6)
+print(result)
