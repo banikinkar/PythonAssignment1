91a5a0fcc0bbca7c00c11efbe74d2f33e853b324
banikinkar <Kinkar.pani@gmail.com>
2026-02-23 18:03:58 +0530

    file operation

---
+++ File_Exception_Handling/filecheck.py
@@ -0,0 +1,26 @@
+#os.path.exists
+
+"""
+Small program to check file exists
+
+import os
+
+filename = "C:/Users/Bani Kinkar Pani/PycharmProjects/PythonProject/File_Exception_Handling/newfile.txt"
+if os.path.exists(filename):
+    print("file exists")
+else:
+    print("file does not exist")
+"""
+
+#pathlib.Path.exists()
+
+from pathlib import Path
+filename = Path("C:/Users/Bani Kinkar Pani/PycharmProjects/PythonProject/File_Exception_Handling/newfile.txt")
+
+if filename.exists():
+    print("file exists")
+else:
+    print("file does not exist")
+
+
+
