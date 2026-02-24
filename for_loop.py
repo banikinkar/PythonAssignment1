b63134b402c3bc4181df94607c7bdfce960ec708
banikinkar <Kinkar.pani@gmail.com>
2026-02-24 19:57:31 +0530

    json file operation module

--- for_loop.py
+++ for_loop.py
@@ -16,13 +16,30 @@ for i in s:
     print(i)
 print("end of loop")
 """
-student1 = {"name":"John" , "age":30, "subject":"Eng"}
+# student1 = {"name":"John" , "age":30, "subject":"Eng"}
+#
+# #for i in student1:
+#  #   print(i,student1[i])
+# #print(student1.items())
+# for i in student1.items():
+#     print(i[0], i[1])
 
-#for i in student1:
- #   print(i,student1[i])
-#print(student1.items())
-for i in student1.items():
-    print(i[0], i[1])
+#
+# str = input("Enter a word in lower case: ")
+# l1=[]
+# l2=[]
+# for i in str:
+#     l1.append(i)
+#
+# #print(l1)
+# for c in str:
+#     if l1.count(c) < 2:# and (l1[c] > l1[c+1]):
+#        l2.append(c)
+# print(l2[0])
+
+# str = input("Enter a string: ")
+# print(str[::-1])
+
 
 
 
