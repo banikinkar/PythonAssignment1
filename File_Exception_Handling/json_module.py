c62252cd1fa0db69e7f822308511b58906eb3016
banikinkar <Kinkar.pani@gmail.com>
2026-02-28 07:32:38 +0530

    json file operation module

--- File_Exception_Handling/json_module.py
+++ File_Exception_Handling/json_module.py
@@ -1,33 +1,49 @@
 import json
 
-students = {'student1':{'roll':101,'name':'John','percent':98.5,'sport':True},
+students = {'student1':{'roll':101,'name':'John','percent':99,'sport':False},
             'student2':{'roll':108,'name':'Carol','percent':78.5,'sport':True},
             'student3':{'roll':91,'name':'Smith','percent':92,'sport':True},
             'student4':{'roll':50,'name':'Mona','percent':80,'sport':True},
             'student5':{'roll':78,'name':'Alex','percent':67,'sport':True}}
+
 print(students)
 print(type(students))
 
-#dump() --> used to convert file for serialization
-# with open("students_data.json","w") as fh:
-#     json.dump(students,fh,indent=5)
-
-#reading use load() ---> used to deserialisation
-
-# with open("students_data.json","r") as fh:
-#     json_data = json.load(fh)
+# #dump() --> used to convert file for serialization
+# # with open("students_data.json","w") as fh:
+# #     json.dump(students,fh,indent=5)
+#
+# #reading use load() ---> used to deserialisation
+#
+# # with open("students_data.json","r") as fh:
+# #     json_data = json.load(fh)
+# #     print(json_data)
+# #     print(type(json_data))
+#
+# ###update() -->updates the dictionary data
+#
+# with open('students_data.json', 'r') as fh:
+#     json_data=json.load(fh)
+#     # json_data.update({'student6':{'roll':101,'name':'John','percent':98.5,'sport':True}})
+#     json_data.update()
+#
+# with open('students_data.json', 'w') as fh:
+#     json.dump(json_data, fh,indent=5)
 #     print(json_data)
-#     print(type(json_data))
 
-###update() -->updates the dictionary data
+
+
+# with open('students_data.json', 'w') as fh:
+#     json.dump(students, fh,indent=5)
 
 with open('students_data.json', 'r') as fh:
-    json_data=json.load(fh)
-    # json_data.update({'student6':{'roll':101,'name':'John','percent':98.5,'sport':True}})
-    json_data.update()
+        data=json.load(fh)
+        data.update(students)
 
 with open('students_data.json', 'w') as fh:
-    json.dump(json_data, fh,indent=5)
-    print(json_data)
+    json.dump(data, fh,indent=4)
+
+
+
 
 
