75227c0646a1c020b2276dd412c033c449cd35db
banikinkar <Kinkar.pani@gmail.com>
2026-02-28 10:19:30 +0530

    rafactored module wise

--- 03_File_Exception_Handling/filecheck.py
+++ 03_File_Exception_Handling/filecheck.py
@@ -5,7 +5,7 @@ Small program to check file exists
 
 import os
 
-filename = "C:/Users/Bani Kinkar Pani/PycharmProjects/PythonProject/File_Exception_Handling/newfile.txt"
+filename = "C:/Users/Bani Kinkar Pani/PycharmProjects/PythonProject/03_File_Exception_Handling/newfile.txt"
 if os.path.exists(filename):
     print("file exists")
 else:
@@ -15,7 +15,7 @@ else:
 #pathlib.Path.exists()
 
 from pathlib import Path
-filename = Path("C:/Users/Bani Kinkar Pani/PycharmProjects/PythonProject/File_Exception_Handling/newfile.txt")
+filename = Path("/03_File_Exception_Handling/newfile.txt")
 
 if filename.exists():
     print("file exists")
