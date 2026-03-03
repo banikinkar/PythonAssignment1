1459fe41e1b60c594f7dfd3c1f9e513ef688fc3b
banikinkar <Kinkar.pani@gmail.com>
2026-03-03 19:02:33 +0530

    oops concept

---
+++ OOAD/oops_in_python.py
@@ -0,0 +1,10 @@
+# # classes are bluprint or design of an objects
+#  Object are entity created using class method
+# Objects == instances of a class
+
+class House:
+    pass
+obj1 = House()
+obj2 = House()
+print(type(obj1))
+
